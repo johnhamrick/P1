b95431085819b7faee4f0c999a44a9062787f3b2
John Hamrick <john@hamrickjohn.me>
2014-10-06 15:28:21 -0400

    Added P2 live URL

--- index.php
+++ index.php
@@ -6,9 +6,7 @@
 	<meta charset="utf-8">
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
 	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
-
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css' type='text/css'>
